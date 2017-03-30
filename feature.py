112b27f8628de02ff032cf6f0e4e16fdbc8c6bdc
dsqx71 <dsqx71@gmail.com>
2017-03-30 14:38:25 +0800

    * Add ExtractWeatherRawdata function * Support multiprocessing during feature extraction

--- feature.py
+++ feature.py
@@ -2,7 +2,9 @@ from util import GetTimeslot
 from datetime import datetime, timedelta
 from numba import jit
 import os
+import platform
 import pandas as pd
+import multiprocessing as mp
 from config import cfg
 from util import ReadJson, WriteJson, ReadRawdata
 
@@ -50,6 +52,7 @@ def ExtractVolumeRawdata(data, volume_feature, time_interval):
         has_etc = data.loc[i]['has_etc']
         vehicle_type = data.loc[i]['vehicle_type']
         time = data.loc[i]['time']
+
         time_slot = GetTimeslot(time, time_interval)
         volume_feature[tollgate][time_slot]['num_direction:{}'.format(direction)] = \
             volume_feature[tollgate][time_slot].get('num_direction:{}'.format(direction),0) + 1
@@ -60,7 +63,22 @@ def ExtractVolumeRawdata(data, volume_feature, time_interval):
         volume_feature[tollgate][time_slot]['num_vehicle_type:{}'.format(vehicle_type)] = \
             volume_feature[tollgate][time_slot].get('num_vehicle_type:{}'.format(vehicle_type), 0) + 1
         volume_feature[tollgate][time_slot]['num'] = volume_feature[tollgate][time_slot].get('num', 0) + 1
+        volume_feature[tollgate][time_slot]['data_miss'] = 0
 
+@jit
+def ExtractWeatherRawdata(weather, weather_feature, time_interval):
+
+    data = weather
+    for i in range(len(data)):
+        date = data.loc[i]['date']
+        hour = data.loc[i]['hour']
+        time = datetime.strptime("%s %02d:00:00" % (date, hour), "%Y-%m-%d %H:%M:%S")
+        for j in range(int(3*60/time_interval)):
+            time_slot = GetTimeslot(time, time_interval)
+            weather_feature[time_slot] = data.loc[i].to_dict()
+            time = time + timedelta(minutes=time_interval)
+    keys = weather_feature.keys()
+    assert len(set(keys)) == len(keys)
 
 @jit
 def PreprocessingRawdata(force_update=False):
@@ -68,48 +86,69 @@ def PreprocessingRawdata(force_update=False):
     # file path
     volume_feature_file = os.path.join(cfg.data.feature_dir, 'volume_feature.json')
     trajectory_feature_file = os.path.join(cfg.data.feature_dir, 'trajectory_feature.json')
+    weather_feature_file = os.path.join(cfg.data.feature_dir, 'weather_feature.json')
 
     # read existing files
-    if os.path.exists(volume_feature_file) and os.path.exists(trajectory_feature_file) and force_update is False:
+    if os.path.exists(volume_feature_file) and \
+       os.path.exists(trajectory_feature_file) and \
+       os.path.exists(weather_feature_file) and force_update is False:
         volume_feature = ReadJson(volume_feature_file)
         trajectory_feature = ReadJson(trajectory_feature_file)
-        return volume_feature, trajectory_feature
+        weather_feature = ReadJson(weather_feature_file)
+    else:
+        # init feature dict
+        trajectory_feature = {}
+        volume_feature = {}
+        weather_feature = {}
 
-    # init feature dict
-    trajectory_feature = {}
-    volume_feature = {}
-
-    for node in cfg.model.link:
-        # trajectory
-        trajectory_feature[node] = {}
-        for slot in cfg.time.all_timeslots:
-            trajectory_feature[node][slot] = {}
-
-        # volume
-        if 'tollgate' in node:
-            volume_feature[node] = {}
+        for node in cfg.model.link:
+            # trajectory
+            trajectory_feature[node] = {}
             for slot in cfg.time.all_timeslots:
-                volume_feature[node][slot] = {}
+                trajectory_feature[node][slot] = {}
 
-    # Extract features
-    trajectory, volume, weather, link, route = ReadRawdata()
-    ExtractTrajectoryRawdata(trajectory, trajectory_feature, time_interval=cfg.time.time_interval)
-    ExtractVolumeRawdata(volume, volume_feature, time_interval=cfg.time.time_interval)
+            # volume
+            if 'tollgate' in node:
+                volume_feature[node] = {}
+                for slot in cfg.time.all_timeslots:
+                    volume_feature[node][slot] = {}
 
-    # Save features
-    WriteJson(volume_feature_file, volume_feature)
-    WriteJson(trajectory_feature_file, trajectory_feature)
+        for slot in cfg.time.all_timeslots:
+            weather_feature[slot] = {}
 
-    return volume_feature, trajectory_feature
+        # Extract features
+        trajectory, volume, weather, link, route = ReadRawdata()
+
+        # multiprocessing setting
+        if platform.system() == 'Windows':
+            mp.freeze_support()
+
+        # Extract feature
+        pool = mp.Pool()
+        pool.apply_async(ExtractTrajectoryRawdata, (trajectory, trajectory_feature, cfg.time.time_interval))
+        pool.apply_async(ExtractVolumeRawdata, (volume, volume_feature, cfg.time.time_interval))
+        pool.apply_async(ExtractWeatherRawdata, (weather, weather_feature, cfg.time.time_interval))
+        pool.close()
+        pool.join()
+
+        # Save features
+        WriteJson(volume_feature_file, volume_feature)
+        WriteJson(trajectory_feature_file, trajectory_feature)
+        WriteJson(weather_feature_file, weather_feature)
+
+    return volume_feature, trajectory_feature, weather_feature
 
 @jit
-def ReformatData(volume_feature, trajectory_feature):
+def ReformatData(volume_feature, trajectory_feature, weather_feature):
 
     data = trajectory_feature.copy()
+    data['weather'] = weather_feature
+
     for node in volume_feature:
         for timeslot in volume_feature[node]:
             for feature in volume_feature[node][timeslot]:
                 data[node][timeslot]['volume_{}'.format(feature)] = volume_feature[node][timeslot][feature]
+
     for key in data:
         data[key] = pd.DataFrame(data[key]).transpose()
 
--- pipeline.py
+++ pipeline.py
@@ -1,10 +1,9 @@
-from config import cfg
-import util
 import feature
 
 if __name__ == '__main__':
     # step1 : preprocessing data, reformat, Fill missing data
-    volume_feature, trajectory_feature = feature.PreprocessingRawdata(force_update=True)
-    data = feature.ReformatData(volume_feature, trajectory_feature)
+    volume_feature, trajectory_feature, weather_feature = feature.PreprocessingRawdata(force_update=True)
+    data = feature.ReformatData(volume_feature, trajectory_feature, weather_feature)
 
     # step2 : training
+
